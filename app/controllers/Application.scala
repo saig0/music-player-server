6cc1601946dcd18615dfc6a2a9990e11c518eb19
Philipp <philipp.ossler@gmail.com>
2014-01-19 00:38:25 +0100

    API für Steuerung des Players hinzugefügt

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,33 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import controllers.music.MusicPlayer
+import controllers.music.MusicLibrary
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+
+	val musicPlayer = MusicPlayer()
+	val musicLibrary = new MusicLibrary()
+
+	musicLibrary.addSource("""E:\Eigene Dateien\Eigene Musik\Lindsey Stirling""")
+	musicPlayer.add(musicLibrary.musics)
+
+	def index = Action {
+		Ok(views.html.index("Your new application is ready."))
+	}
+
+	def getCurrentPlaylist = Action {
+		val currentPlaylist = musicPlayer.currentPlaylist
+		Ok(currentPlaylist.toString)
+	}
+
+	def playMusic = Action {
+		musicPlayer.play
+		Ok
+	}
+
+	def pauseMusic = Action {
+		musicPlayer.pause
+		Ok
+	}
 }
